99b3cacfbdb85c7f39f38480dad738b16eff79d8
HarryNhu <harry.nhu@vmodev.com>
2023-02-07 23:04:28 +0700

    fix: update package

--- src/webparts/readReceiptWebpart/ReadReceiptWebpartWebPart.ts
+++ src/webparts/readReceiptWebpart/ReadReceiptWebpartWebPart.ts
@@ -1,120 +1,143 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+import { Version } from '@microsoft/sp-core-library'
+import { IPropertyPaneConfiguration, PropertyPaneTextField } from '@microsoft/sp-property-pane'
+import { BaseClientSideWebPart, WebPartContext } from '@microsoft/sp-webpart-base'
+import { IReadonlyTheme } from '@microsoft/sp-component-base'
+
+import * as strings from 'ReadReceiptWebpartWebPartStrings'
+import ReadReceiptWebpart from './components/ReadReceiptWebpart'
+import { IReadReceiptWebpartProps } from './components/IReadReceiptWebpartProps'
+import { sp } from '@pnp/sp/presets/all'
+
 import {
-  IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IReadonlyTheme } from '@microsoft/sp-component-base';
-
-import * as strings from 'ReadReceiptWebpartWebPartStrings';
-import ReadReceiptWebpart from './components/ReadReceiptWebpart';
-import { IReadReceiptWebpartProps } from './components/IReadReceiptWebpartProps';
-
+	PropertyFieldListPicker,
+	PropertyFieldListPickerOrderBy,
+} from '@pnp/spfx-property-controls/lib/PropertyFieldListPicker'
 export interface IReadReceiptWebpartWebPartProps {
-  description: string;
+	documentTitle: string
+	currentUserDisplayName: string
+	storageList: string
+	acknowledgementLabel: string
+	acknowledgementMessage: string
+	readMessage: string
+	themeVariant: IReadonlyTheme | undefined
+	configured: boolean
+	context: WebPartContext
 }
 
 export default class ReadReceiptWebpartWebPart extends BaseClientSideWebPart<IReadReceiptWebpartWebPartProps> {
+	private _isDarkTheme: boolean = false
+	private _environmentMessage: string = ''
 
-  private _isDarkTheme: boolean = false;
-  private _environmentMessage: string = '';
+	public render(): void {
+		const element: React.ReactElement<IReadReceiptWebpartProps> = React.createElement(
+			ReadReceiptWebpart,
+			{
+				// description: this.properties.description,
+				// isDarkTheme: this._isDarkTheme,
+				// environmentMessage: this._environmentMessage,
+				// hasTeamsContext: !!this.context.sdks.microsoftTeams,
+				// userDisplayName: this.context.pageContext.user.displayName
+				documentTitle: this.properties.documentTitle,
+				currentUserDisplayName: this.properties.currentUserDisplayName,
+				storageList: this.properties.storageList,
+				acknowledgementLabel: this.properties.acknowledgementLabel,
+				acknowledgementMessage: this.properties.acknowledgementMessage,
+				readMessage: this.properties.readMessage,
+				themeVariant: this.properties.themeVariant,
+				configured: this.properties.configured,
+				context: this.properties.context,
+			}
+		)
 
-  public render(): void {
-    const element: React.ReactElement<IReadReceiptWebpartProps> = React.createElement(
-      ReadReceiptWebpart,
-      {
-        description: this.properties.description,
-        isDarkTheme: this._isDarkTheme,
-        environmentMessage: this._environmentMessage,
-        hasTeamsContext: !!this.context.sdks.microsoftTeams,
-        userDisplayName: this.context.pageContext.user.displayName
-      }
-    );
+		ReactDom.render(element, this.domElement)
+	}
 
-    ReactDom.render(element, this.domElement);
-  }
+	protected onInit(): Promise<void> {
+		return this._getEnvironmentMessage().then((message) => {
+			this._environmentMessage = message
+		})
+	}
 
-  protected onInit(): Promise<void> {
-    return this._getEnvironmentMessage().then(message => {
-      this._environmentMessage = message;
-    });
-  }
+	private _getEnvironmentMessage(): Promise<string> {
+		if (!!this.context.sdks.microsoftTeams) {
+			// running in Teams, office.com or Outlook
+			return this.context.sdks.microsoftTeams.teamsJs.app.getContext().then((context) => {
+				let environmentMessage: string = ''
+				switch (context.app.host.name) {
+					case 'Office': // running in Office
+						environmentMessage = this.context.isServedFromLocalhost
+							? strings.AppLocalEnvironmentOffice
+							: strings.AppOfficeEnvironment
+						break
+					case 'Outlook': // running in Outlook
+						environmentMessage = this.context.isServedFromLocalhost
+							? strings.AppLocalEnvironmentOutlook
+							: strings.AppOutlookEnvironment
+						break
+					case 'Teams': // running in Teams
+						environmentMessage = this.context.isServedFromLocalhost
+							? strings.AppLocalEnvironmentTeams
+							: strings.AppTeamsTabEnvironment
+						break
+					default:
+						throw new Error('Unknown host')
+				}
 
+				return environmentMessage
+			})
+		}
 
+		return Promise.resolve(
+			this.context.isServedFromLocalhost
+				? strings.AppLocalEnvironmentSharePoint
+				: strings.AppSharePointEnvironment
+		)
+	}
 
-  private _getEnvironmentMessage(): Promise<string> {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
-      return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
-        .then(context => {
-          let environmentMessage: string = '';
-          switch (context.app.host.name) {
-            case 'Office': // running in Office
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
-              break;
-            case 'Outlook': // running in Outlook
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
-              break;
-            case 'Teams': // running in Teams
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
-              break;
-            default:
-              throw new Error('Unknown host');
-          }
+	protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+		if (!currentTheme) {
+			return
+		}
 
-          return environmentMessage;
-        });
-    }
+		this._isDarkTheme = !!currentTheme.isInverted
+		const { semanticColors } = currentTheme
 
-    return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
-  }
+		if (semanticColors) {
+			this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null)
+			this.domElement.style.setProperty('--link', semanticColors.link || null)
+			this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null)
+		}
+	}
 
-  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
-    if (!currentTheme) {
-      return;
-    }
+	protected onDispose(): void {
+		ReactDom.unmountComponentAtNode(this.domElement)
+	}
 
-    this._isDarkTheme = !!currentTheme.isInverted;
-    const {
-      semanticColors
-    } = currentTheme;
+	protected get dataVersion(): Version {
+		return Version.parse('1.0')
+	}
 
-    if (semanticColors) {
-      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
-      this.domElement.style.setProperty('--link', semanticColors.link || null);
-      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
-    }
-
-  }
-
-  protected onDispose(): void {
-    ReactDom.unmountComponentAtNode(this.domElement);
-  }
-
-  protected get dataVersion(): Version {
-    return Version.parse('1.0');
-  }
-
-  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-    return {
-      pages: [
-        {
-          header: {
-            description: strings.PropertyPaneDescription
-          },
-          groups: [
-            {
-              groupName: strings.BasicGroupName,
-              groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
-    };
-  }
+	protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+		return {
+			pages: [
+				{
+					header: {
+						description: strings.PropertyPaneDescription,
+					},
+					groups: [
+						{
+							groupName: strings.BasicGroupName,
+							groupFields: [
+								PropertyPaneTextField('description', {
+									label: strings.DescriptionFieldLabel,
+								}),
+							],
+						},
+					],
+				},
+			],
+		}
+	}
 }
--- src/webparts/readReceiptWebpart/components/IReadReceiptWebpartProps.ts
+++ src/webparts/readReceiptWebpart/components/IReadReceiptWebpartProps.ts
@@ -1,7 +1,13 @@
+import { IReadonlyTheme } from '@microsoft/sp-component-base'
+import { WebPartContext } from '@microsoft/sp-webpart-base'
 export interface IReadReceiptWebpartProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
+	documentTitle: string
+	currentUserDisplayName: string
+	storageList: string
+	acknowledgementLabel: string
+	acknowledgementMessage: string
+	readMessage: string
+	themeVariant: IReadonlyTheme | undefined
+	configured: boolean
+	context: WebPartContext
 }
